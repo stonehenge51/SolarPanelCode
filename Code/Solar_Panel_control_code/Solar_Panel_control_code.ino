21b493d28ed59a8fa277a526f3da8ff4ce4632ff
stonehenge51 <stonehenge515@gmail.com>
2022-02-09 13:01:38 -0800

    added basic on off motor control

--- Code/Solar_Panel_control_code/Solar_Panel_control_code.ino
+++ Code/Solar_Panel_control_code/Solar_Panel_control_code.ino
@@ -1,14 +1,39 @@
-#define out 2
-#define len 500
+#define direction_signal_M1 2
+#define pulse_signal_M1 3
+#define on_signal_M1 4
+
+#define direction_signal_M2 5
+#define pulse_signal_M2 6
+#define on_signal_M2 7
+
+#define len 1
+
+int val = 0; 
+
 void setup() {
-  pinMode(out, OUTPUT);
+  pinMode(direction_signal_M1, OUTPUT);
+  pinMode(pulse_signal_M1, OUTPUT);
+  pinMode(on_signal_M1, INPUT);
+
+  pinMode(direction_signal_M2, OUTPUT);
+  pinMode(pulse_signal_M2, OUTPUT);
+  pinMode(on_signal_M2, INPUT);
 
 }
 
 void loop() {
-  digitalWrite(out, HIGH); // sets the digital pin 13 on
-  delay(len);            // waits for a second
-  digitalWrite(out, LOW);  // sets the digital pin 13 off
-  delay(len);  
+  val = digitalRead(on_signal_M1);
+  if(val == HIGH){
+    digitalWrite(direction_signal_M1, HIGH);
+    delay(1);
+    digitalWrite(pulse_signal_M1, HIGH);
+    delay(len);
+    digitalWrite(pulse_signal_M1, LOW);
+    delay(len);  
+  }
+  if(val == HIGH){
+    digitalWrite(direction_signal_M1, LOW);
+  }
+  
 
 }
